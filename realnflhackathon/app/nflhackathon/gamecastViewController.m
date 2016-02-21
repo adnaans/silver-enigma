97f3c3eda5785d34a52a54946640d8d30943389a
kevinfang2 <kevkev0123@gmail.com>
2016-02-21 04:32:06 -0800

    same

--- realnflhackathon/app/nflhackathon/gamecastViewController.m
+++ realnflhackathon/app/nflhackathon/gamecastViewController.m
@@ -18,6 +18,7 @@
     __weak IBOutlet UIView *otherView;
     __weak IBOutlet UIBarButtonItem *last;
     __weak IBOutlet UIBarButtonItem *next;
+    __weak IBOutlet UILabel *time;
 }
 
 
@@ -27,13 +28,12 @@
 
 #define _width self.view.frame.size.width
 #define _height self.view.frame.size.height
-#define gridwidth otherView.frame.size.width/100
-#define gridheight otherView.frame.size.height/100
+#define gridwidth otherView.frame.size.width/120
+#define gridheight otherView.frame.size.height/120
 
 #define startingline self.view.frame.size.height/1.1
+
 int file=0;
-
-
 
 - (IBAction)next:(id)sender {
     if(_indexcheck == 0){
@@ -44,6 +44,8 @@ int file=0;
         }
         else{
             file = file + 5;
+            NSLog(@"same is %d",file);
+
 
             last.enabled = YES;
         }
@@ -82,39 +84,39 @@ int file=0;
 }
 
 
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    
     
 //    miniUniform.alpha = 0;
     
     [[[[Firebase alloc] initWithUrl:@"https://nflhackathonthingy.firebaseio.com"] childByAppendingPath:@"file"] setValue:[NSNumber numberWithInt:file]];
     Firebase *myRootRef;
-    
-    [myRootRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
-        NSLog(@"%@ -> %@", snapshot.key, snapshot.value);
-    }];
-    
-    int numberofplayer=11;
     float x, y;
-    
-    [self drawPath:x,y];
 
-    if(_indexcheck == 0){
-        NSError *error;
-        NSString *fileName = @"%d.json",*file; // name of the PDF you are searching for
-        NSDictionary* json = [NSJSONSerialization JSONObjectWithData:fileName options:0 error:&error];
-        
-        AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-        [manager POST:@"https://nflhackathonthingy.firebaseio.com/test" parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-            NSLog(@"JSON: %@", responseObject);
-        } failure:^(NSURLSessionTask *operation, NSError *error) {
-            NSLog(@"Error: %@", error);
+    bool isModal;
+    
+    while(isModal == true){
+        [myRootRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+            NSLog(@"%@ -> %@", snapshot.key, snapshot.value);
+            
+            __block x = [snapshot.value[@"x"] floatValue];
+            __block y = [snapshot.value[@"y"] floatValue];
         }];
         
+        NSLog(@"BAHAHBHAHBAHABHABHABH %.2f",x);
+        NSLog(@"%.2f",y);
+        
+        [self drawPath:x,y];
     }
+
     
+    
+    if(_indexcheck == 0){
+
+    }
     else if (_indexcheck == 1){
         // use game 2 data
     }
@@ -124,8 +126,14 @@ int file=0;
     // Do any additional setup after loading the view.
 }
 
+- (BOOL)isModal {
+    return self.presentingViewController.presentedViewController == self
+    || (self.navigationController != nil && self.navigationController.presentingViewController.presentedViewController == self.navigationController)
+    || [self.tabBarController.presentingViewController isKindOfClass:[UITabBarController class]];
+}
+
 -(void)animateStrokeEnd:(CGFloat)_strokeEnd {
-    
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -133,26 +141,40 @@ int file=0;
     // Dispose of any resources that can be recreated.
 }
 //put after drawPath when ir works
-- (void)drawPath:(CGFloat)_xlocation,ylocation
+- (void)drawPath:(CGFloat)xlocation,ylocation
 {
+    Firebase *ref = [[Firebase alloc] initWithUrl: @"https://nflhackathonthingy.firebaseio.com"];
+    int numberofplayer=11;
 
-    UIBezierPath *path = [UIBezierPath bezierPath];
-    [path moveToPoint:CGPointMake(_width/4, startingline)];
-    [path addLineToPoint:CGPointMake(100.0, 100.0)];
-    [path addLineToPoint:CGPointMake(150.0, 200)];
-    
-    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
-    shapeLayer.path = [path CGPath];
-    shapeLayer.strokeColor = [[UIColor blackColor] CGColor];
-    shapeLayer.lineWidth = 2.0;
-    shapeLayer.fillColor = [[UIColor clearColor] CGColor];
+    [[ref queryOrderedByChild:@"derp/gsisPlayId"]
+     observeEventType:FEventTypeChildAdded withBlock:^(FDataSnapshot *snapshot) {
+         NSLog(@"They're doing %@ meters", snapshot.value);
+         NSLog(@"%@", snapshot.value[@"title"]);
 
-    [self.view.layer addSublayer:shapeLayer];
+//         time.text = (@"%@", snapshot.value[@"time"]);
+         for(int index = 0; index<=numberofplayer; index++){
+             UIBezierPath *path = [UIBezierPath bezierPath];
+             [path moveToPoint:CGPointMake(50, startingline)];
+             [path addLineToPoint:CGPointMake(xlocation, ylocation)];
+//             [path addLineToPoint:CGPointMake(x, y)];
+             
+             CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+             shapeLayer.path = [path CGPath];
+             shapeLayer.strokeColor = [[UIColor blackColor] CGColor];
+             shapeLayer.lineWidth = 2.0;
+             shapeLayer.fillColor = [[UIColor clearColor] CGColor];
+             
+             [self.view.layer addSublayer:shapeLayer];
+             
+             UIImage* image = [UIImage imageNamed:@"miniUniform"];
+             UIImageView* miniJersey = [[UIImageView alloc] initWithFrame:CGRectMake(50, 100.0, 50, 30)];
+             miniJersey.image = image;
+             [self.view addSubview:miniJersey];
+         }
+     }];
     
-    UIImage* image = [UIImage imageNamed:@"miniUniform"];
-    UIImageView* miniJersey = [[UIImageView alloc] initWithFrame:CGRectMake(50, 100.0, 50, 30)];
-    miniJersey.image = image;
-    [self.view addSubview:miniJersey];
+    
+    
 }
 /*
 #pragma mark - Navigation
